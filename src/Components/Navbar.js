e45b7db2d51c737dba35af31a9db06a61b0eff94
Waleed Ahmad <90566825+wal33dahmad@users.noreply.github.com>
2022-05-07 18:55:01 +0500

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="logo">
+        <a className="logo_name" href="/">
+          XCASH CENTRAL
+        </a>
+      </div>
+      <ul className="nav_links">
+        <li className="nav_link">XCASH MERCH</li>
+        <li className="nav_link">Delegates</li>
+        <li className="nav_link">X-CASH NEWS</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
